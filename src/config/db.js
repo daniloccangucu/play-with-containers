aa208c907e423c3f4a18bbab39adbaa5d9bb2101
Danilo Canguçu <daniloccangucu@gmail.com>
2024-05-16 18:53:28 +0300

    debug added for vm

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = new pg.Pool({
+  user: process.env.PGUSER,
   // uncomment the following line to use a local database
-  // user: process.env.PGUSER,
   // password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
   host: process.env.PGHOST,
   port: process.env.PGPORT,
 });
+
+console.log("database:", process.env.PGDATABASE);
+console.log("host:", process.env.PGHOST);
+console.log("port:", process.env.PGPORT);
