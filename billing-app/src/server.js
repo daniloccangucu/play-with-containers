dd75ee27fcbb04caf77f79e4c51d87b0589c7aa9
Danilo Canguçu <daniloccangucu@gmail.com>
2024-05-24 21:38:49 +0300

    rabbitmq connecting with billing-app

--- billing-app/src/server.js
+++ billing-app/src/server.js
@@ -14,20 +14,22 @@ app.get("/health", (_req, res) => {
   res.status(200).send("OK");
 });
 
-// connectToRabbitMQ()
-//   .then(() => {
-//     console.log("Connected to RabbitMQ");
-//   })
-//   .catch((err) => {
-//     console.error("Failed to connect to RabbitMQ, proceeding without it:", err);
-//   })
-//   .finally(() => {
-app.listen(PORT, () => {
-  console.log(`Billing API is running on port ${PORT}`);
-  console.log(`Access within Docker network: http://billing-app:${PORT}`);
-  console.log(`Health check available at: http://billing-app:${PORT}/health`);
-});
-// })
-// .catch((error) => {
-//   console.error("Failed to start Billing API server:", error);
-// });
+connectToRabbitMQ()
+  .then(() => {
+    console.log("Connected to RabbitMQ");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to RabbitMQ, proceeding without it:", err);
+  })
+  .finally(() => {
+    app.listen(PORT, () => {
+      console.log(`Billing API is running on port ${PORT}`);
+      console.log(`Access within Docker network: http://billing-app:${PORT}`);
+      console.log(
+        `Health check available at: http://billing-app:${PORT}/health`
+      );
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to start Billing API server:", error);
+  });
