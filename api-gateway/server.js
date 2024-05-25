03b47e93124789887cebf129526dcea6726e6e9d
Danilo Canguçu <daniloccangucu@gmail.com>
2024-05-25 15:13:52 +0300

    added ipv6 in gateway-api

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -11,13 +11,14 @@ const { json } = pkg;
 const app = express();
 const PORT = process.env.GATEWAY_PORT;
 const INVENTORY_API_URL = process.env.INVENTORY_API_URL;
+const GATEWAY_HOST = process.env.GATEWAY_HOST;
 
 app.use(json());
 
 app.use("/api/movies", proxy);
 app.use("/api", routes);
 
-app.listen(PORT, () => {
+app.listen(PORT, GATEWAY_HOST, () => {
   console.log(`API Gateway running on port ${PORT}`);
   console.log(`API Gateway is accessible at http://localhost:${PORT}`);
   console.log(`The Inventory API is proxied from ${INVENTORY_API_URL}`);
