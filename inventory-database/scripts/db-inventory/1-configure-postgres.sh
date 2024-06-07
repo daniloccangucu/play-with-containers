68b17de633f277edbcf6e9b5ef33ab3f23286f9a
Danilo Canguçu <daniloccangucu@gmail.com>
2024-06-07 19:23:20 +0300

    inventory-database code in ECS

--- inventory-database/scripts/db-inventory/1-configure-postgres.sh
+++ inventory-database/scripts/db-inventory/1-configure-postgres.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -e
 
-# Ensure the script is executable
-chmod +x /docker-entrypoint-initdb.d/1-configure-postgres.sh
-
-# Create the SQL file with the password change command
-echo "ALTER USER postgres WITH PASSWORD '$PGPASSWORD';" > /docker-entrypoint-initdb.d/change_password.sql
+# Create the SQL file with the password change command in /tmp directory
+echo "ALTER USER postgres WITH PASSWORD '$PGPASSWORD';" > /tmp/change_password.sql
 
 # Run the SQL file as the postgres user
-psql -U postgres -f /docker-entrypoint-initdb.d/change_password.sql
+psql -U postgres -f /tmp/change_password.sql
 
 # Clean up by removing the SQL file
-rm /docker-entrypoint-initdb.d/change_password.sql
+rm /tmp/change_password.sql
